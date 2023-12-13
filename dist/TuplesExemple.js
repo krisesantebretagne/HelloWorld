e6d7f6a5bb4eb6407d8a2c449ff775dfd9ba3e14
Krisandra ADAMS <krisandra.adams@esante-bretagne.fr>
2023-12-13 11:32:47 +0100

    Ajouté TuplesExemple

---
+++ dist/TuplesExemple.js
@@ -0,0 +1,5 @@
+"use strict";
+let user = [1, 'Kris'];
+console.log("id : " + user[0]);
+console.log("nom : " + user[1]);
+//# sourceMappingURL=TuplesExemple.js.map
---
+++ src/TuplesExemple.ts
@@ -0,0 +1,7 @@
+/* Tuple: array d'un longueur défini, types définis.
+Bon pour les pairs clé/valeur */
+
+//id: 1, nom: 'Kris'
+let user: [number, string] = [1, 'Kris'];
+console.log("id : " + user[0]);
+console.log("nom : " + user[1]);
