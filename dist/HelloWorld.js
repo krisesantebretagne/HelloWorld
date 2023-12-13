4155f8501d4cc8fe8be53e7856c82ce45dc2c034
Krisandra ADAMS <krisandra.adams@esante-bretagne.fr>
2023-12-13 10:26:51 +0100

    Ajouté sourceMap = true à tsconfig.json pour débugger et créé nouveau fichier pour tester les fonctions de debug

--- dist/HelloWorld.js
+++ dist/HelloWorld.js
@@ -1,3 +1,4 @@
 "use strict";
 let message = "Hello, world!";
 console.log(message);
+//# sourceMappingURL=HelloWorld.js.map
---
+++ src/TypeScriptDebugTest.ts
@@ -0,0 +1,5 @@
+//Pour tester les fonctions debug 
+let age: number = 20;
+if (age < 50)
+    age +=10;
+console.log(age);
